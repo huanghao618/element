a1b88ed964feef5819bb5d96eefeee752eec9390
huanghao618 <1098385916@qq.com>
2020-11-09 19:48:13 +0800

    提交

--- src/pages/Good.vue
+++ src/pages/Good.vue
@@ -100,14 +100,33 @@ export default {
     },
     // 删除
     delGood(item) {
-      // console.log(item)
+       // console.log(item)
       let params = {
         id: item
       };
-      this.$http.delGood(params).then(() => {
-        
-        this.getHotGoodList();
+       this.$http.delGood(params).then(() => {
+          this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center: true
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+           this.getHotGoodList();
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
       });
+       
+      
+     
+     
     },
     // 分页
     handleCurrentChange: function(page) {
